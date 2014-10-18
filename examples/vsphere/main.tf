23df7675fd67405cfb3aa7ad71be26d23a801042
Gavin Gray <gavin@infinio.com>
2014-10-18 18:06:34 -0400

    Failing test: terraform apply

---
+++ examples/vsphere/main.tf
@@ -0,0 +1,9 @@
+# Specify the provider and access details
+provider "vsphere" {
+    # region = "${var.aws_region}"
+    hostname = "192.168.238.137"
+}
+
+#resource "datacenter" {
+#  name = "terraform-test"
+#}
